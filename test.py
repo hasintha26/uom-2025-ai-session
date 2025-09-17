26d74c97c3083e03712a596d9d85b5c40a1eceb3
Hasintha26 <koshalhasinthajaya@gmail.com>
2025-09-17 18:01:28 +0530

    add trunk to tree

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print('hi')
+print(" "*6,'christmas tree\n')
+
+print(" "*11,"*")
+for i in range(11):
+   print(f'{" "*(11-i)}{"*"*(i)}{"***"}{"*"*(i)}')
+print(" "*10,"*"*3)
+print(" "*9,"*"*5)
+print(" "*8,"*"*7)
+print(" "*7,"*"*9)
