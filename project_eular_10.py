d81c0a0bb3a24a2f3c6d31427d83170b19805cad
Hasintha26 <koshalhasinthajaya@gmail.com>
2025-09-23 11:49:35 +0530

    solve project eular problem 10

---
+++ project_eular_10.py
@@ -0,0 +1,12 @@
+def isprime(num):
+    if num ==1:
+        return False
+    for i in range(2,int(num**0.5)+1):
+        if num%i==0:
+            return False
+    return True
+sum=0
+for i in range(2,2000000):
+    if isprime(i):
+        sum+=i
+print(sum)
